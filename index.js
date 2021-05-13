4b7d427f0190240796e8e9f4e8cf1b5f0e7894f3
Nate <7nater.nelson@gmail.com>
2021-05-13 12:39:58 -0600

    commit tree f04b1d0fd70b594e1b3ebe6c1ba891f554d1e54b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function wrapAdjective(flair = "*"){
+    return function(adj = "special"){
+        return "You are " + flair + adj + flair + "!" 
+    }
+}
+
+let encouragingPromptFunction = wrapAdjective("!!!")
+
+function saturdayFun(activity = "roller-skate"){
+    return "This Saturday, I want to " + activity + "!"
+}
+
+function mondayWork(activity = "go to the office"){
+    return "This Monday, I will " + activity + "."
+}
+
+const Calculator = {
+    add: (a,b) => a+b,
+    subtract: (a,b) => a-b,
+    multiply: (a,b) => a*b,
+    divide: (a,b) => a/b,
+}
+
+function actionApplyer(int=0, arr){
+    if( !arr || arr.length == 0) return int
+    for(let func of arr) {
+        int = func(int)}
+    return int
+}
